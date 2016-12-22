b7c9bfe7568567e66580a898f304a996158fc450
Stephen Greene <sgreene570@gmail.com>
2016-12-22 00:35:30 -0500

    initial CLI version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # conwaylife 
+Simple implementation of conways game of life in python.
+This is really just for fun, I plan on making this more efficient.
+I also want to add a nice gui and other functionality.
+
 
---
+++ life.py
@@ -0,0 +1,43 @@
+# Conways game of life in python
+# @author: Stephen Greene
+
+import argparse
+import random
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Conway's game of life")
+    parser.add_argument('grid_size', type=int, help='side of square grid')
+    size = int(parser.parse_args().grid_size)
+    grid = [[random.getrandbits(1) for x in range(0, size)] for y in range(0, size)]
+    while True:
+        turn(grid)
+        for x in range(0, size):
+            print(grid[x])
+            print()
+        time.sleep(1)
+
+
+def turn(grid):
+    size = len(grid)
+    for x in range(0, size):
+        for y in range(0, size):
+            neighbors = 0;
+            for a in range(0, 2):
+                for b in range(0, 2):
+                    if x + a > 0 and x + a < size and y + b > 0 and y + b < size:
+                        if grid[x + a][y + b] == 1:
+                            neighbors += 1
+
+            if neighbors < 2:
+                grid[x][y] = 0
+            elif neighbors == 3:
+                grid[x][y] = 1
+            elif neighbors > 3:
+                grid[x][y] = 0
+
+
+
+if __name__ == "__main__":
+    main()
