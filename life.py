f39e16fe43384672ef301abc86a4065ef98ba268
Stephen Greene <sgreene570@gmail.com>
2016-12-22 00:41:47 -0500

    fixed neighbor finding

--- life.py
+++ life.py
@@ -10,7 +10,8 @@ def main():
     parser = argparse.ArgumentParser(description="Conway's game of life")
     parser.add_argument('grid_size', type=int, help='side of square grid')
     size = int(parser.parse_args().grid_size)
-    grid = [[random.getrandbits(1) for x in range(0, size)] for y in range(0, size)]
+    grid = [[random.getrandbits(1) for x in range(0, size)]
+        for y in range(0, size)]
     while True:
         turn(grid)
         for x in range(0, size):
@@ -24,11 +25,11 @@ def turn(grid):
     for x in range(0, size):
         for y in range(0, size):
             neighbors = 0;
-            for a in range(0, 2):
-                for b in range(0, 2):
-                    if x + a > 0 and x + a < size and y + b > 0 and y + b < size:
-                        if grid[x + a][y + b] == 1:
-                            neighbors += 1
+            for a in range(-1, 3, 2):       #Iterate through surrounding cells
+                for b in range(-1, 3, 2):
+                    if (x + a > 0 and x + a < size and y + b > 0 and
+                        y + b < size and grid[x + a][y +b] == 1):
+                        neighbors += 1
 
             if neighbors < 2:
                 grid[x][y] = 0
