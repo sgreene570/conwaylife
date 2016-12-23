68ff3a21b540b7c9bb04dbbd7dfa275179336cd8
Stephen Greene <sgreene570@gmail.com>
2016-12-23 14:39:24 -0500

    added simple gui

---
+++ gui.py
@@ -0,0 +1,32 @@
+# Game of life GUI
+# @author Stephen Greene
+
+
+import tkinter
+import argparse
+import random
+import life
+import time
+
+
+def main():
+    root = tkinter.Tk()
+    parser = argparse.ArgumentParser(description="Conway's game of life")
+    parser.add_argument('grid_size', type=int, help='side of square grid')
+    size = int(parser.parse_args().grid_size)
+    grid = [[random.getrandbits(1) for x in range(0, size)]
+        for y in range(0, size)]
+    while(True):
+        for r in range(0, size):
+            for c in range(0, size):
+                tkinter.Label(
+                root, text=str(grid[r][c]) + " ", borderwidth=1).grid(row=r,
+                column=c)
+
+        grid = life.turn(grid)
+        time.sleep(1)
+        root.update()
+
+
+if __name__ == "__main__":
+    main()
--- life.py
+++ life.py
@@ -9,6 +9,10 @@ import random
 import time
 
 
+DEAD_CELL = 0
+LIVE_CELL = 1
+
+
 def main():
     parser = argparse.ArgumentParser(description="Conway's game of life")
     parser.add_argument('grid_size', type=int, help='side of square grid')
@@ -28,18 +32,21 @@ def turn(grid):
     for x in range(0, size):
         for y in range(0, size):
             neighbors = 0;
-            for a in range(-1, 3, 2):       #Iterate through surrounding cells
-                for b in range(-1, 3, 2):
-                    if (x + a > 0 and x + a < size and y + b > 0 and
-                        y + b < size and grid[x + a][y +b] == 1):
+            for a in range(-1, 2):       #Iterate through surrounding cells
+                for b in range(-1, 2):
+                    if (x + a >= 0 and x + a < size and y + b >= 0 and
+                        y + b < size and grid[x + a][y + b] == LIVE_CELL
+                        and (a != 0 and b!= 0)):
                         neighbors += 1
 
             if neighbors < 2:
-                grid[x][y] = 0
+                grid[x][y] = DEAD_CELL
             elif neighbors == 3:
-                grid[x][y] = 1
+                grid[x][y] = LIVE_CELL
             elif neighbors > 3:
-                grid[x][y] = 0
+                grid[x][y] = DEAD_CELL
+
+    return grid
 
 
 if __name__ == "__main__":
